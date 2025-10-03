39ea8df167d5c7f511fdbf544c3f2fd9ffbde148
Jiab77 <jiab77@pm.me>
2025-10-03 04:57:31 +0200

    New release 0.2.0

--- README.md
+++ README.md
@@ -5,60 +5,76 @@ Hide (_mostly_) anything in plain sight.
 ## Features
 
 * Handle several file types (audio, video, image, pdf)
-* Can hide a string, a file and a folder
+* Can hide a string, a file or a folder
 * Fileless (avoids writing to disk as possible)
 * Encryption (AES 256 / ChaCha20-Poly1305)
-  
+* Accept password from prompt or environment variable
+* Use auto generated key if no password provided
+
 > [!NOTE]
 > Folder content will be archived as `zip` files and embedded into the target file.
 
+## How it works
+
+Basically, I'm (ab)using [metadata](https://en.wikipedia.org/wiki/Metadata) tags embedded in audio, video, image and pdf files to store the encrypted payload passed as argument or from stdin.
+
+More precisely, I'm creating (or replacing) two metadata tags:
+
+1. Comment (_used to store detection tag line_)
+2. Description (_used to store the encrypted payload_)
+
+The payload is encrypted and base64 encoded before being stored to the `description` metadata tag.
+
+For the decryption, the script will parse the `description` metadata tag if it finds the defined tag line in the `comment` metadata tag.
+
+> [!NOTE]
+> You can change the detection tag line in the [source code](cloak.sh) of the script.
+
 ## Limits
 
-Contrary to the real steganography, (ab)using files metadata has some limits that may not exist with the [LSB](https://www.researchgate.net/publication/368691521_Hiding_secret_data_in_audiovideoimagetext_steganography_using_least_significant_bit_algorithmMHIndia) algorithm.
-
-### Hidden data size
-
-Basically, the limit for storing hidden data will be the target file size itself.
-
-So, if your target file size is 500 KB, you will then be able to store (hide) up to 500 KB of data into the target file.
-
-The bigger the target file is, the more you can store (hide) data in it.
-
-> [!TIP]
-> __As a rule of thumb, the data that you want to store (hide) must be smaller than the target file that will hold it.__
+Contrary to the real [steganography](https://en.wikipedia.org/wiki/Steganography), (ab)using files [metadata](https://en.wikipedia.org/wiki/Metadata) tags has some limits that may not exist with the [LSB](https://www.researchgate.net/publication/368691521_Hiding_secret_data_in_audiovideoimagetext_steganography_using_least_significant_bit_algorithmMHIndia) algorithm.
 
 ### Metadata tags
 
 When testing if PDF files could be used as target file, I've discovered that only the `description` tag could be added but not the `comment` tag.
 
 > [!NOTE]
-> I've then modified the code to handle this situation but depending on the target file format, the script might fail to write the required metadata tags.
+> I then modified the code to handle this situation but depending on the target file format, the script might fail to write the required metadata tags.
+>
+> Please, create an issue with the unsupported file type so that I can try to implement required code.
 
 ### Not very stealthy
 
 Another limit is that the encrypted data can be seen while using an hexadecimal viewer like `xxd`.
 
 > [!IMPORTANT]
-> If that's a problem for you, you should then fallback on real steganography to conceal your secret data.
+> If that's a problem for you, you should then fallback on real [steganography](https://en.wikipedia.org/wiki/Steganography) to conceal your secret data.
 
 ## Usage
 
 ```console
-Usage: cloak.sh <file> <data | string> - Embed and Hide data in file.
+
+Usage: cloak.sh <file> [payload] - Embed and Hide data in file.
 
 Arguments:
 
-  -h | --help                        Print this help message
-  -d | --dump <file>                 Dump data from given file
-  -e | --extract <file>              Extract data from given file
-  -k | --keep                        Keep original input file (don't replace it)
+  -h | --help                             Print this help message
+  -d | --dump <file>                      Dump data from given file
+  -e | --extract <file>                   Extract data from given file
+  -k | --keep                             Keep original input file (don't replace it)
+  -p | --pass                             Enable password protection
 
 Examples:
 
-  * cloak.sh <file>                  Print file tags
-  * cloak.sh <file> <data | string>  Embed and Hide data in file tags
-  * cloak.sh -d <file>               Read given file tags and print hidden data
-  * cloak.sh -e <file>               Read given file tags and extract hidden data
+  * cloak.sh <file>                      Print file tags
+  * cloak.sh <file> <payload>            Embed and Hide data in file tags
+  * cat <file> | cloak.sh <file> -       Embed and Hide data from stdin
+  * echo <string> | cloak.sh <file> -    Embed and Hide string from stdin
+  * cloak.sh -d <file>                   Read given file tags and print hidden data
+  * cloak.sh -d <file> | file -          Read given file tags and get hidden data type
+  * cloak.sh -e <file>                   Read given file tags and extract hidden data
+
+Note: The payload can be either a file, a string or a folder.
 
 ```
 
--- cloak.sh
+++ cloak.sh
@@ -9,19 +9,20 @@
 # Made by Jiab77
 #
 # Notes:
-# - Part of this work has been inspired by the work done by THC.
+# - Part of this project has been inspired by the work done by THC.
 #
-# Version 0.1.0
+# Version 0.2.0
 
 # Options
-[[ -r $HOME/.debug ]] && set -o xtrace || set +o xtrace
+[[ -e $HOME/.debug ]] && set -x
 
 # Config
 ENC_ALGO_OSSL="chacha20"
 ENC_ALGO_GPG="aes256"
+TAG_LINE="Modified by Cloak"
 ALLOW_FOLDERS=true
 KEEP_ORIGINAL=false
-TAG_LINE="Modified by Cloak"
+USE_PASSWORD=false
 RUN_MODE="embed"  # Available run modes: 'embed', 'extract', 'dump'
 
 # Internals
@@ -46,6 +47,9 @@ BIN_7Z=$(which 7z 2>/dev/null)
 function die() {
   echo -e "\nError: $1\n" >&2
   exit 255
+}
+function log() {
+  echo "$@" >&2
 }
 function print_missing() {
   local MISSING_COUNT=0
@@ -72,21 +76,27 @@ function print_usage() {
 function print_help() {
   cat >&2 <<EOF
 
-Usage: $SCR_NAME <file> <data | string> - Embed and Hide data in file.
+Usage: $SCR_NAME <file> [payload] - Embed and Hide data in file.
 
 Arguments:
 
-  -h | --help                         Print this help message
-  -d | --dump <file>                  Dump data from given file
-  -e | --extract <file>               Extract data from given file
-  -k | --keep                         Keep original input file (don't replace it)
+  -h | --help                             Print this help message
+  -d | --dump <file>                      Dump data from given file
+  -e | --extract <file>                   Extract data from given file
+  -k | --keep                             Keep original input file (don't replace it)
+  -p | --pass                             Enable password protection
 
 Examples:
 
-  * $SCR_NAME <file>                  Print file tags
-  * $SCR_NAME <file> <data | string>  Embed and Hide data in file tags
-  * $SCR_NAME -d <file>               Read given file tags and print hidden data
-  * $SCR_NAME -e <file>               Read given file tags and extract hidden data
+  * $SCR_NAME <file>                      Print file tags
+  * $SCR_NAME <file> <payload>            Embed and Hide data in file tags
+  * cat <file> | $SCR_NAME <file> -       Embed and Hide data from stdin
+  * echo <string> | $SCR_NAME <file> -    Embed and Hide string from stdin
+  * $SCR_NAME -d <file>                   Read given file tags and print hidden data
+  * $SCR_NAME -d <file> | file -          Read given file tags and get hidden data type
+  * $SCR_NAME -e <file>                   Read given file tags and extract hidden data
+
+Note: The payload can be either a file, a string or a folder.
 
 EOF
 
@@ -98,30 +108,59 @@ function enc_str() {
 function dec_str() {
   echo -n "$1" | base64 -d -
 }
+function gen_key() {
+  if [[ ! -r $SCR_DIR/key.dat ]]; then
+    openssl rand -base64 32 > "$SCR_DIR/key.dat"
+  fi
+}
 function enc_data() {
   if [[ -n $BIN_OSSL ]]; then
-    echo -n "$1" | openssl "$ENC_ALGO_OSSL" -pbkdf2 -e -in - | base64 -w0 -
+    if [[ -n $PASSWORD ]]; then
+      echo -n "$1" | openssl "$ENC_ALGO_OSSL" -pbkdf2 -e -pass env:PASSWORD -in - | base64 -w0 -
+    elif [[ $USE_PASSWORD == false ]]; then
+      echo -n "$1" | openssl "$ENC_ALGO_OSSL" -pbkdf2 -e -pass file:"$SCR_DIR/key.dat" -in - | base64 -w0 -
+    else
+      echo -n "$1" | openssl "$ENC_ALGO_OSSL" -pbkdf2 -e -in - | base64 -w0 -
+    fi
   else
     echo -n "$1" | gpg -ac --cipher-algo="$ENC_ALGO_GPG" -o- | base64 -w0 -
   fi
 }
 function dec_data() {
   if [[ -n $BIN_OSSL ]]; then
-    echo -n "$1" | base64 -d - | openssl "$ENC_ALGO_OSSL" -pbkdf2 -d -in -
+    if [[ -n $PASSWORD ]]; then
+      echo -n "$1" | base64 -d - | openssl "$ENC_ALGO_OSSL" -pbkdf2 -d -pass env:PASSWORD -in -
+    elif [[ $USE_PASSWORD == false ]]; then
+      echo -n "$1" | base64 -d - | openssl "$ENC_ALGO_OSSL" -pbkdf2 -d -pass file:"$SCR_DIR/key.dat" -in -
+    else
+      echo -n "$1" | base64 -d - | openssl "$ENC_ALGO_OSSL" -pbkdf2 -d -in -
+    fi
   else
     echo -n "$1" | base64 -d - | gpg -o-
   fi
 }
 function enc_file() {
   if [[ -n $BIN_OSSL ]]; then
-    openssl "$ENC_ALGO_OSSL" -pbkdf2 -e -in "$1" | base64 -w0 -
+    if [[ -n $PASSWORD ]]; then
+      openssl "$ENC_ALGO_OSSL" -pbkdf2 -e -pass env:PASSWORD -in "$1" | base64 -w0 -
+    elif [[ $USE_PASSWORD == false ]]; then
+      openssl "$ENC_ALGO_OSSL" -pbkdf2 -e -pass file:"$SCR_DIR/key.dat" -in "$1" | base64 -w0 -
+    else
+      openssl "$ENC_ALGO_OSSL" -pbkdf2 -e -in "$1" | base64 -w0 -
+    fi
   else
     gpg -ac --cipher-algo="$ENC_ALGO_GPG" -o- "$1" | base64 -w0 -
   fi
 }
 function dec_file() {
   if [[ -n $BIN_OSSL ]]; then
-    base64 -d - | openssl "$ENC_ALGO_OSSL" -pbkdf2 -d -in - > "/tmp/$1"
+    if [[ -n $PASSWORD ]]; then
+      base64 -d - | openssl "$ENC_ALGO_OSSL" -pbkdf2 -d -pass env:PASSWORD -in - > "/tmp/$1"
+    elif [[ $USE_PASSWORD == false ]]; then
+      base64 -d - | openssl "$ENC_ALGO_OSSL" -pbkdf2 -d -pass file:"$SCR_DIR/key.dat" -in - > "/tmp/$1"
+    else
+      base64 -d - | openssl "$ENC_ALGO_OSSL" -pbkdf2 -d -in - > "/tmp/$1"
+    fi
   else
     base64 -d - | gpg -o- > "/tmp/$1"
   fi
@@ -138,10 +177,10 @@ function cmp_folder() {
   ARCHIVE_NAME="$(basename "$1").zip"
   if [[ -d "$1" ]]; then
     if [[ -n $BIN_ZIP && ! -r "/tmp/$ARCHIVE_NAME" ]]; then
-      echo -e "\nCompressing folder [$1] to [$ARCHIVE_NAME]...\n"
+      log -e "\nCompressing folder [$1] to [$ARCHIVE_NAME]...\n"
       ( cd "$1" && zip -r - -9 . ) > "/tmp/$ARCHIVE_NAME"
     elif [[ -n $BIN_7Z && ! -r "/tmp/$ARCHIVE_NAME" ]]; then
-      echo -e "\nCompressing folder [$1] to [$ARCHIVE_NAME]...\n"
+      log -e "\nCompressing folder [$1] to [$ARCHIVE_NAME]...\n"
       ( cd "$1" && 7z a -tzip -so -mx9 . ) > "/tmp/$ARCHIVE_NAME"
     else
       die "Missing required 'zip' compression tool."
@@ -156,25 +195,29 @@ function check_file_size() {
   fi
 }
 function get_diff() {
-  echo -e "\nShowing changes...\n"
+  log -e "\nShowing changes...\n"
   exiftool -P "$1" -diff "$2"
 }
 function get_tags_ex() {
-  echo -e "\nReading file tags...\n"
+  log -e "\nReading file tags...\n"
   exiftool -P "$1" -json | jq .
 }
 function get_tags_ff() {
-  echo -e "\nReading file tags...\n"
+  log -e "\nReading file tags...\n"
   ffprobe -loglevel error -show_entries stream_tags:format_tags -of json "$1" | jq .
+}
+function get_input_format() {
+  ffprobe -loglevel error -show_format -of json "$1" | jq -rc ".format.format_name"
 }
 function hide_data_ex() {
   local INPUT_DIR ; INPUT_DIR="$(dirname "$1")"
   local INPUT_NAME ; INPUT_NAME="$(basename "$1")"
+  local INPUT_TAGS ; INPUT_TAGS="/tmp/${INPUT_NAME}.tags"
   local OUTPUT_NAME ; OUTPUT_NAME="${INPUT_NAME}.mod"
   local ARCHIVE_NAME ; ARCHIVE_NAME="$(basename "$2").zip"
 
   if [[ ! $OUTPUT_NAME == "$INPUT_NAME" && -e "${INPUT_DIR}/${OUTPUT_NAME}" ]]; then
-    echo -e "\nRemoving existing file '$INPUT_DIR/$OUTPUT_NAME'...\n"
+    log -e "\nRemoving existing file '$INPUT_DIR/$OUTPUT_NAME'...\n"
     del_file "${INPUT_DIR}/${OUTPUT_NAME}"
   fi
 
@@ -184,71 +227,106 @@ function hide_data_ex() {
   # 1) A linearized PDF file is no longer linearized after the update, so it must be subsequently re-linearized if this is required.
   # 2) All metadata edits are reversible. While this would normally be considered an advantage, it is a potential security problem because old information is never actually deleted from the file. (However, after running ExifTool the old information may be removed permanently using the "qpdf" utility with this command: "qpdf --linearize in.pdf out.pdf".)
 
-  echo -e "\nAdding data to file...\n"
+  log -e "\nAdding data to file...\n"
   if [[ -f "$2" ]]; then
-    check_file_size "$1" "$2"
-    exiftool -P -comment="$TAG_LINE" -description="N:$(enc_str "$(basename "$2")");F:$(enc_file "$2")" -o "${INPUT_DIR}/${OUTPUT_NAME}" "$1" || die "Unable to embed and encrypt data in file."
+    # Create file tags
+    echo -ne "-comment=${TAG_LINE}\n-description=N:$(enc_str "$(basename "$2")");F:" > "$INPUT_TAGS"
+    enc_file "$2" >> "$INPUT_TAGS"
+
+    # Write tags from file
+    exiftool -P -@ "$INPUT_TAGS" -o "${INPUT_DIR}/${OUTPUT_NAME}" "$1" || die "Unable to embed and encrypt data in file."
     get_diff "$1" "${INPUT_DIR}/${OUTPUT_NAME}"
   elif [[ -d "$2" && $ALLOW_FOLDERS == true ]]; then
     if [[ $(cmp_folder "$2") ]]; then
       if [[ -f "/tmp/$ARCHIVE_NAME" ]]; then
-        check_file_size "$1" "/tmp/$ARCHIVE_NAME"
-        hide_data_ex "$1" "/tmp/$ARCHIVE_NAME"
+        # Recall function with new archive file
+        "${FUNCNAME[0]}" "$1" "/tmp/$ARCHIVE_NAME"
         del_file "/tmp/$ARCHIVE_NAME"
       fi
     fi
   elif [[ $2 == "-" ]]; then
-    exiftool -P -comment="$TAG_LINE" -description="S:$(enc_file /dev/stdin)" -o "${INPUT_DIR}/${OUTPUT_NAME}" "$1" || die "Unable to embed data in file."
+    # Create file tags
+    echo -ne "-comment=${TAG_LINE}\n-description=N:$(enc_str stdin);F:" > "$INPUT_TAGS"
+    enc_file /dev/stdin >> "$INPUT_TAGS"
+
+    # Write tags from stdin
+    exiftool -P -@ "$INPUT_TAGS" -o "${INPUT_DIR}/${OUTPUT_NAME}" "$1" || die "Unable to embed and encrypt data in file."
     get_diff "$1" "${INPUT_DIR}/${OUTPUT_NAME}"
   else
-    exiftool -P -comment="$TAG_LINE" -description="S:$(enc_data "$2")" -o "${INPUT_DIR}/${OUTPUT_NAME}" "$1" || die "Unable to embed data in file."
+    # Write tags from string
+    exiftool -P \
+             -comment="$TAG_LINE" \
+             -description="S:$(enc_data "$2")" \
+             -o "${INPUT_DIR}/${OUTPUT_NAME}" "$1" \
+             || die "Unable to embed data in file."
+
+    # Show file diff
     get_diff "$1" "${INPUT_DIR}/${OUTPUT_NAME}"
   fi
-  if [[ -r "${INPUT_DIR}/${OUTPUT_NAME}" && $KEEP_ORIGINAL == false ]]; then
-    mv -fv "${INPUT_DIR}/${OUTPUT_NAME}" "${INPUT_DIR}/${INPUT_NAME}"
+
+  # Clean
+  [[ -e "$INPUT_TAGS" ]] && rm -f "$INPUT_TAGS"
+  if [[ -e "${INPUT_DIR}/${OUTPUT_NAME}" && $KEEP_ORIGINAL == false ]]; then
+    mv -f "${INPUT_DIR}/${OUTPUT_NAME}" "${INPUT_DIR}/${INPUT_NAME}"
   fi
 }
 function hide_data_ff() {
   local INPUT_DIR ; INPUT_DIR="$(dirname "$1")"
   local INPUT_NAME ; INPUT_NAME="$(basename "$1")"
+  local INPUT_TAGS ; INPUT_TAGS="/tmp/${INPUT_NAME}.tags"
+  local OUTPUT_FORMAT ; OUTPUT_FORMAT="$(get_input_format "$1")"
   local OUTPUT_NAME ; OUTPUT_NAME="${INPUT_NAME}.mod"
   local ARCHIVE_NAME ; ARCHIVE_NAME="$(basename "$2").zip"
 
-  echo -e "\nAdding data to file...\n"
+  log -e "\nAdding data to file...\n"
   if [[ -f "$2" ]]; then
-    check_file_size "$1" "$2"
-    ffmpeg -hide_banner \
-           -y -i "$1" \
+    # Create file tags
+    echo -ne ";FFMETADATA1\ncomment=${TAG_LINE}\ndescription=N:$(enc_str "$(basename "$2")");F:" > "$INPUT_TAGS"
+    enc_file "$2" >> "$INPUT_TAGS"
+
+    # Write tags from file
+    ffmpeg -hide_banner -y \
+           -i "$1" \
+           -i "$INPUT_TAGS" \
            -c copy \
            -movflags use_metadata_tags \
            -map_metadata 0 \
-           -metadata comment="$TAG_LINE" \
-           -metadata description="N:$(enc_str "$(basename "$2")");F:$(enc_file "$2")" \
+           -map_metadata 1 \
+           -f "$OUTPUT_FORMAT" \
            "${INPUT_DIR}/${OUTPUT_NAME}" \
            || die "Unable to embed and encrypt data in file."
 
+    # Show file diff
     get_diff "$1" "${INPUT_DIR}/${OUTPUT_NAME}"
   elif [[ -d "$2" && $ALLOW_FOLDERS == true ]]; then
     if [[ $(cmp_folder "$2") ]]; then
       if [[ -f "/tmp/$ARCHIVE_NAME" ]]; then
-        check_file_size "$1" "/tmp/$ARCHIVE_NAME"
-        hide_data_ff "$1" "/tmp/$ARCHIVE_NAME"
+        # Recall function with new archive file
+        "${FUNCNAME[0]}" "$1" "/tmp/$ARCHIVE_NAME"
         del_file "/tmp/$ARCHIVE_NAME"
       fi
     fi
   elif [[ $2 == "-" ]]; then
-    ffmpeg -hide_banner \
-           -y -i "$1" \
+    # Create file tags
+    echo -ne ";FFMETADATA1\ncomment=${TAG_LINE}\ndescription=N:$(enc_str stdin);F:" > "$INPUT_TAGS"
+    enc_file /dev/stdin >> "$INPUT_TAGS"
+
+    # Write tags from stdin
+    ffmpeg -hide_banner -y \
+           -i "$1" \
+           -i "$INPUT_TAGS" \
            -c copy \
            -movflags use_metadata_tags \
            -map_metadata 0 \
-           -metadata comment="$TAG_LINE" \
-           -metadata description="S:$(enc_file /dev/stdin)" \
+           -map_metadata 1 \
+           -f "$OUTPUT_FORMAT" \
            "${INPUT_DIR}/${OUTPUT_NAME}" \
-           || die "Unable to embed data in file."
+           || die "Unable to embed and encrypt data in file."
 
+    # Show file diff
     get_diff "$1" "${INPUT_DIR}/${OUTPUT_NAME}"
   else
+    # Write tags from string
     ffmpeg -hide_banner \
            -y -i "$1" \
            -c copy \
@@ -256,13 +334,18 @@ function hide_data_ff() {
            -map_metadata 0 \
            -metadata comment="$TAG_LINE" \
            -metadata description="S:$(enc_data "$2")" \
+           -f "$OUTPUT_FORMAT" \
            "${INPUT_DIR}/${OUTPUT_NAME}" \
            || die "Unable to embed data in file."
 
+    # Show file diff
     get_diff "$1" "${INPUT_DIR}/${OUTPUT_NAME}"
   fi
-  if [[ -r "${INPUT_DIR}/${OUTPUT_NAME}" && $KEEP_ORIGINAL == false ]]; then
-    mv -fv "${INPUT_DIR}/${OUTPUT_NAME}" "${INPUT_DIR}/${INPUT_NAME}"
+
+  # Clean
+  [[ -e "$INPUT_TAGS" ]] && rm -f "$INPUT_TAGS"
+  if [[ -e "${INPUT_DIR}/${OUTPUT_NAME}" && $KEEP_ORIGINAL == false ]]; then
+    mv -f "${INPUT_DIR}/${OUTPUT_NAME}" "${INPUT_DIR}/${INPUT_NAME}"
   fi
 }
 function get_data_ex() {
@@ -274,7 +357,7 @@ function get_data_ex() {
   local ONAM
   local JSON
 
-  echo -e "\nReading data from file...\n"
+  echo -e "\nReading data from file '$(basename "$1")'...\n"
   if [[ -f "$1" ]]; then
     JSON=$(exiftool -P "$1" -json | jq -rc .)
     FCOM=$(jq -rc ".[0].Comment" <<< "$JSON")
@@ -314,7 +397,7 @@ function get_data_ff() {
   local ONAM
   local JSON
 
-  echo -e "\nReading data from file...\n"
+  echo -e "\nReading data from file '$(basename "$1")'...\n"
   if [[ -f "$1" ]]; then
     JSON=$(ffprobe -loglevel error -show_entries stream_tags:format_tags -of json "$1" | jq -rc .)
     FCOM=$(jq -rc ".format.tags.comment" <<< "$JSON")
@@ -354,7 +437,7 @@ function print_data_ex() {
   local ONAM
   local JSON
 
-  echo -e "\nReading data from file...\n" >&2
+  log -e "\nReading data from file...\n"
   if [[ -f "$1" ]]; then
     JSON=$(exiftool -P "$1" -json | jq -rc .)
     FCOM=$(jq -rc ".[0].Comment" <<< "$JSON")
@@ -366,14 +449,14 @@ function print_data_ex() {
 
     # FIXME: Tag "comment" is not added in PDF files
     if [[ $FCOM == "$TAG_LINE" || -n $FTAG ]]; then
-      echo -e "Found embedded content:\n" >&2
+      log -e "Found embedded content:\n"
       if [[ $FTAG == "N" ]]; then
         [[ -n $FDAT ]] && dec_data "$FDAT"
       else
         [[ -n $FSTR ]] && dec_data "$FSTR"
       fi
     else
-      echo "Nothing found." >&2
+      log "Nothing found."
     fi
   else
     die "Invalid source file given."
@@ -388,7 +471,7 @@ function print_data_ff() {
   local ONAM
   local JSON
 
-  echo -e "\nReading data from file...\n" >&2
+  log -e "\nReading data from file...\n"
   if [[ -f "$1" ]]; then
     JSON=$(ffprobe -loglevel error -show_entries stream_tags:format_tags -of json "$1" | jq -rc .)
     FCOM=$(jq -rc ".format.tags.comment" <<< "$JSON")
@@ -399,21 +482,21 @@ function print_data_ff() {
     ONAM="$(dec_str "$FNAM")"
 
     if [[ $FCOM == "$TAG_LINE" ]]; then
-      echo -e "Found embedded content:\n" >&2
+      log -e "Found embedded content:\n"
       if [[ $FTAG == "N" ]]; then
         [[ -n $FDAT ]] && dec_data "$FDAT"
       else
         [[ -n $FSTR ]] && dec_data "$FSTR"
       fi
     else
-      echo "Nothing found." >&2
+      log "Nothing found."
     fi
   else
     die "Invalid source file given."
   fi
 }
 function hide_data() {
-  if [[ $1 == *.mp3 ]]; then
+  if [[ $1 == *.mp3 || $1 == *.avi || $1 == *.ogg ]]; then
     hide_data_ff "$1" "$2"
   else
     hide_data_ex "$1" "$2"
@@ -457,6 +540,7 @@ while [[ $# -ne 0 ]]; do
     -d|--debug) RUN_MODE="dump" ; shift ;;
     -e|--extract) RUN_MODE="extract" ; shift ;;
     -k|--keep) KEEP_ORIGINAL=true ; shift ;;
+    -p|--pass) USE_PASSWORD=true ; shift ;;
     *) break ;;
   esac
 done
@@ -468,6 +552,7 @@ done
 
 # Init
 print_missing
+gen_key
 
 # Main
 if [[ $# -eq 1 ]]; then
